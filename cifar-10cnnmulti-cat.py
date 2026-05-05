472196defbe75ffadd1d317f503555dab3e5a69d
Akshaykanna123 <akshay.ai2026@gmail.com>
2026-05-05 09:43:50 +0530

    Create cifar-10cnnmulti-cat.py

---
+++ cifar-10cnnmulti-cat.py
@@ -0,0 +1,27 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.datasets import cifar10
+from keras.utils import to_categorical
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+model = Sequential([
+    Conv2D(32,(3,3),activation='relu',input_shape=(32,32,3)),
+    MaxPooling2D((2,2)),
+    Flatten(),
+    Dense(10,activation='softmax')
+])
+
+model.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=5, batch_size=64)
+
+loss, acc = model.evaluate(X_test, y_test)
+print("Accuracy:", acc)
